fddebc208ed3be0eda053078fc865acf508db5fb
sergiogarcia0050 <segarcia@stone.com.co>
2025-04-11 07:27:39 -0500

    fix: :fire: cambios generales del kamban

--- src/App.tsx
+++ src/App.tsx
@@ -26,29 +26,29 @@ export default function App() {
         <div className="min-h-screen flex flex-col">
           <header className="flex justify-between w-full flex-row p-4">
             <Button variant="link" asChild className="text-primary h-8 w-8 p-0">
-              <a href="https://github.com/Georgegriff/react-dnd-kit-tailwind-shadcn-ui">
+              <a href="https://github.com/JURIVERA2024/kanban-bootcamp">
                 <Github className="fill-current h-full w-full" />
               </a>
             </Button>
             <Button variant="link" asChild className="text-primary h-16 w-16">
-              <a href="https://griffa.dev">@griffadev</a>
+              {/* <a href="https://griffa.dev">@griffadev</a> */}
             </Button>
             <ThemeToggle />
           </header>
 
           <main className="mx-4 flex flex-col gap-6">
             <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-              Drag and Drop Kanban Board
+              Equipo Backend 
             </h1>
             <KanbanBoard />
             <p className="leading-7 [&:not(:first-child)]:mt-6">
-              With Keyboard Controls and Screen Reader interactions.
+              Solo los reales no comeran torta.
             </p>
           </main>
 
           <footer className="mt-6">
             <ul className="flex items-center justify-center">
-              <li>
+              {/* <li>
                 <FooterLink>
                   <a href="https://react.dev/">React</a>
                 </FooterLink>
@@ -67,7 +67,7 @@ export default function App() {
                 <FooterLink>
                   <a href="https://ui.shadcn.com/">shadcn/ui</a>
                 </FooterLink>
-              </li>
+              </li> */}
             </ul>
           </footer>
         </div>
